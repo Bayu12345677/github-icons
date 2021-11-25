47c4213dff71198ec5cfe20d5f9392b9e50459bc
polygon <86620608+Bayu12345677@users.noreply.github.com>
2021-11-25 15:17:52 +0700

    Create .fish

---
+++ file/.fish
@@ -0,0 +1,13 @@
+set name github-icons
+set user Bayu12345677
+set source git-icons.sh
+
+begin
+      cat <<EOF
+{
+   (
+     url="https://github.com/Bayu12345677/github-icons"
+    );
+};
+EOF
+     end
